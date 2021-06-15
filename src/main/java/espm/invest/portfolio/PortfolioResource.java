57b6280352aa1bc80544f331e7d9c40930802fb0
pedroheck <pedroscheck@hotmail.com>
2021-06-15 12:30:39 -0300

    adding currency routes

--- src/main/java/espm/invest/portfolio/PortfolioResource.java
+++ src/main/java/espm/invest/portfolio/PortfolioResource.java
@@ -1,6 +1,7 @@
 package espm.invest.portfolio;
 
 import espm.invest.portfolio.common.controller.PortfolioController;
+import espm.invest.portfolio.common.datatype.CurrencyTransaction;
 import espm.invest.portfolio.common.datatype.Portfolio;
 import espm.invest.portfolio.common.datatype.StockTransaction;
 import espm.invest.portfolio.common.datatype.TransactionBean;
@@ -16,6 +17,9 @@ public class PortfolioResource implements PortfolioController{
 
     @Autowired
     private StockTransactionService stockTransactionService;
+
+    @Autowired
+    private CurrencyTransactionService currencyTransactionService;
 
     @Override
     public Portfolio portfolio(String id) {
@@ -42,5 +46,15 @@ public class PortfolioResource implements PortfolioController{
     @Override
     public StockTransaction stockSell(String idPortfolio, TransactionBean bean) {
         return stockTransactionService.sell(idPortfolio, bean);
+    }
+
+    @Override
+    public CurrencyTransaction currencyBuy(String idPortfolio, TransactionBean bean) {
+        return currencyTransactionService.buy(idPortfolio, bean);
+    }
+
+    @Override
+    public CurrencyTransaction currencySell(String idPortfolio, TransactionBean bean) {
+        return currencyTransactionService.sell(idPortfolio, bean);
     }
 }
